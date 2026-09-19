0c8dba7a3b1319f85db2206458a7bfa2457b8545
hsudais201-ops <hsudais201@gmail.com>
2026-09-19 14:45:40 +0500

    fix: import dialog spacing units

--- app/src/main/java/com/example/ui/screens/LoaderInstallDialog.kt
+++ app/src/main/java/com/example/ui/screens/LoaderInstallDialog.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
