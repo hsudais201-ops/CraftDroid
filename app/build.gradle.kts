3e970ae5bbaa8ee2910cfbd32e461df5b4ed6cda
hsudais201-ops <hsudais201@gmail.com>
2026-09-19 14:45:08 +0500

    build: add Compose material pull refresh support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
   implementation(libs.androidx.compose.material.icons.core)
   implementation(libs.androidx.compose.material.icons.extended)
   implementation(libs.androidx.compose.material3)
+  implementation("androidx.compose.material:material")
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.graphics)
   implementation(libs.androidx.compose.ui.tooling.preview)
